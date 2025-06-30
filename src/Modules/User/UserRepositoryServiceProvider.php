265fd637bdadf6cb227f2c13c742406aedfc9067
Ed Bedia <fordbdia@gmail.com>
2025-06-30 11:45:15 -0500

    Implement User module

---
+++ src/Modules/User/UserRepositoryServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SharedResources\Modules\User;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
