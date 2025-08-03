430e70a457f8c516b72f7e80a8cfcc012fb260cf
Edford Patrick Bedia <edbedia@Edfords-MacBook-Pro.local>
2025-08-02 21:51:35 -0500

    Fixed `stripe_customer_id` column migration

--- src/Modules/User/Database/Migrations/2024_07_01_000000_create_users_table.php
+++ src/Modules/User/Database/Migrations/2024_07_01_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('stripe_customer_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('stripe_customer_id')->nullable()->after('email');
             $table->rememberToken();
             $table->timestamps();
         });
