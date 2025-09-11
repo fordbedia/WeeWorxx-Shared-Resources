487c0ec060be73877aa0c1e492e95ce925748334
Ed Bedia <fordbedia@gmail.com>
2025-09-10 20:12:15 -0500

    Implement custom ApiException class for the search API

---
+++ src/Modules/Post/Exception/SearchApiException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WeeWorxxSDK\SharedResources\Modules\Post\Exception;
+
+use WeeWorxxSDK\SharedResources\SDK\Exception\ApiException;
+
+class SearchApiException extends ApiException
+{
+
+}
--- src/Modules/Post/Search/PostSearch.php
+++ src/Modules/Post/Search/PostSearch.php
@@ -2,7 +2,9 @@
 
 namespace WeeWorxxSDK\SharedResources\Modules\Post\Search;
 
+use Illuminate\Database\QueryException;
 use WeeWorxxSDK\SharedResources\Modules\Post\DTO\PostSearchCriteria;
+use WeeWorxxSDK\SharedResources\Modules\Post\Exception\SearchApiException;
 use WeeWorxxSDK\SharedResources\Modules\Post\Repository\Contracts\PostRepositoryInterface;
 use WeeWorxxSDK\SharedResources\Modules\Post\Repository\Eloquents\PostRepository;
 use WeeWorxxSDK\SharedResources\Modules\Post\Resources\SearchPostResource;
@@ -15,15 +17,26 @@ class PostSearch
 
 	public function __construct(protected PostSearchEngine $engine){}
 
+	/**
+	 * @throws SearchApiException
+	 */
 	public function search(PostSearchCriteria $criteria)
 	{
 		$sql = $this->engine->constructQuery($criteria)
 			->build();
 
-		$paginator = PostRepository::sqlToEloquent($sql)->with($this->postRelation)
-			->active()
-			->paginate(10)
-			->through(fn ($post) => (new SearchPostResource($post))->toArray(request()));
+		try {
+			$paginator = PostRepository::sqlToEloquent($sql)->with($this->postRelation)
+				->active()
+				->paginate(10)
+				->through(fn($post) => (new SearchPostResource($post))->toArray(request()));
+		} catch(QueryException $exception) {
+			throw new SearchApiException(
+					"SQL Malformed: Please check your query string if it's using the correct keywords.",
+						400,
+						'sql_malformed'
+			);
+		}
 
 		return array_merge($paginator->toArray(), [
 			'sql_query' => $sql,
